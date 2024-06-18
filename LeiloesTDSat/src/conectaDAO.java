25f1ff90f17b49051e617234a47308e0eab86614
CristianeMMS <crism2silva@gmail.com>
2024-06-18 17:26:33 -0300

    Funcionalidades cadastro e listagem de produtos

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -4,30 +4,42 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
+
+    public Connection conn; 
+
+    private String url = "jdbc:mysql://localhost:3306/leiloestdsat"; 
+    private String user = "root"; 
+    private String password = "!753SqL951";  
     
-    public Connection connectDB(){
-        Connection conn = null;
+    public boolean conectar(){
         
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão realizada com sucesso");
+            return true;
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Falha na conexão com o banco "+ ex.getMessage());
+            return false;
         }
-        return conn;
     }
-    
+
+public void desconectar(){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao desconectar" + ex.getMessage());
+        
+        }
+}
+
 }
